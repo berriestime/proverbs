7870238bcfb4d3e4774001dd22f9d12fac6bbab2
Galina-babayan <gal-babayan@yandex.ru>
2023-02-15 18:43:32 +0300

    1

---
+++ assets/scripts/modal.js
@@ -0,0 +1,203 @@
+let json = `[{
+
+    "Image": "../images/bragel1.jpg",
+    "number": 1,
+    
+    "discriptionRight": "В 1559 году, когда создавалось это произведение, красный считался цветом греха, а голубой часто означал обман или глупость. ",
+   
+    "discription": "Но некоторые фразы требуют разъяснений. Например, метла, точащая из окна («выставить метлу»), означает «развлекаться в отсутствии хозяина», а целующаяся парочка под ней («жениться под метлой») — сожительство без брака. Выражение «крыть крышу (сладкими) пирогами» подразумевает чьё-то богатство; «дыра в (чьей-то) крыше» — глупость.",
+    "discription2": "В этой части картины можно увидеть знакомые нам поговорки «как карта ляжет» (падающие карты), «жребий брошен» (игральные кости рядом с картами), «дуракам везёт» (или «дуракам идёт карта»), «смотреть сквозь пальцы», «водить друг друга за нос», «мочиться против ветра» (во фламандском — «мочиться на луну») и «гадить на мир», а также «мир перевернулся вверх тормашками» (перевёрнутый глобус с крестом внизу).",
+    "discription3": "Но некоторые фразы требуют разъяснений. Например, метла, точащая из окна («выставить метлу»), означает «развлекаться в отсутствии хозяина», а целующаяся парочка под ней («жениться под метлой») — сожительство без брака. Выражение «крыть крышу (сладкими) пирогами» подразумевает чьё-то богатство; «дыра в (чьей-то) крыше» — глупость.",
+    "discription4": "Но некоторые фразы требуют разъяснений. Например, метла, точащая из окна («выставить метлу»), означает «развлекаться в отсутствии хозяина», а целующаяся парочка под ней («жениться под метлой») — сожительство без брака. Выражение «крыть крышу (сладкими) пирогами» подразумевает чьё-то богатство; «дыра в (чьей-то) крыше» — глупость.",
+    "back1": "у стен есть уши",
+    "back2": "старая крыша требует множества заплаток",
+    "back3": "дуракам идёт карта"
+   
+  },
+  {
+    "Image": "../images/braigel3.jpg",
+    "number": 2,
+    
+    "discriptionRight": "В 1559 году, когда создавалось это произведение, красный считался цветом греха, а голубой часто означал обман или глупость. ",
+   
+   
+    "discription3": "В этой части картины можно увидеть знакомые нам поговорки «как карта ляжет» (падающие карты), «жребий брошен» (игральные кости рядом с картами), «дуракам везёт» (или «дуракам идёт карта»), «смотреть сквозь пальцы», «водить друг друга за нос», «мочиться против ветра» (во фламандском — «мочиться на луну») и «гадить на мир», а также «мир перевернулся вверх тормашками» (перевёрнутый глобус с крестом внизу).",
+    "discription2": "Но некоторые фразы требуют разъяснений. Например, метла, точащая из окна («выставить метлу»), означает «развлекаться в отсутствии хозяина», а целующаяся парочка под ней («жениться под метлой») — сожительство без брака. Выражение «крыть крышу (сладкими) пирогами» подразумевает чьё-то богатство; «дыра в (чьей-то) крыше» — глупость.",
+    "discription4": "Но некоторые фразы требуют разъяснений. Например, метла, точащая из окна («выставить метлу»), означает «развлекаться в отсутствии хозяина», а целующаяся парочка под ней («жениться под метлой») — сожительство без брака. Выражение «крыть крышу (сладкими) пирогами» подразумевает чьё-то богатство; «дыра в (чьей-то) крыше» — глупость.",
+    "back1": "у стен есть уши",
+    "back2": "старая крыша требует множества заплаток",
+    "back3": "дуракам идёт карта"
+  },
+  {
+    "Image": "../images/braigel3.jpg",
+    "number": 3,
+    
+    "discriptionRight": "В 1559 году, когда создавалось это произведение, красный считался цветом греха, а голубой часто означал обман или глупость. ",
+    "discription3": "В этой части картины можно увидеть знакомые нам поговорки «как карта ляжет» (падающие карты), «жребий брошен» (игральные кости рядом с картами), «дуракам везёт» (или «дуракам идёт карта»), «смотреть сквозь пальцы», «водить друг друга за нос», «мочиться против ветра» (во фламандском — «мочиться на луну») и «гадить на мир», а также «мир перевернулся вверх тормашками» (перевёрнутый глобус с крестом внизу).",
+   
+    "discription2": "В этой части картины можно увидеть знакомые нам поговорки «как карта ляжет» (падающие карты), «жребий брошен» (игральные кости рядом с картами), «дуракам везёт» (или «дуракам идёт карта»), «смотреть сквозь пальцы», «водить друг друга за нос», «мочиться против ветра» (во фламандском — «мочиться на луну») и «гадить на мир», а также «мир перевернулся вверх тормашками» (перевёрнутый глобус с крестом внизу).",
+    "discription": "Но некоторые фразы требуют разъяснений. Например, метла, точащая из окна («выставить метлу»), означает «развлекаться в отсутствии хозяина», а целующаяся парочка под ней («жениться под метлой») — сожительство без брака. Выражение «крыть крышу (сладкими) пирогами» подразумевает чьё-то богатство; «дыра в (чьей-то) крыше» — глупость.",
+    "discription4": "Но некоторые фразы требуют разъяснений. Например, метла, точащая из окна («выставить метлу»), означает «развлекаться в отсутствии хозяина», а целующаяся парочка под ней («жениться под метлой») — сожительство без брака. Выражение «крыть крышу (сладкими) пирогами» подразумевает чьё-то богатство; «дыра в (чьей-то) крыше» — глупость.",
+    "back1": "у стен есть уши",
+    "back2": "старая крыша требует множества заплаток",
+    "back3": "дуракам идёт карта"
+  }]`;
+
+let fragments = JSON.parse(json);
+console.log(fragments);
+
+// let arrDiscription = [
+//   {
+//     number: 1,
+//     discription:
+//       "Яйцо в корзине под навесом напоминает о необходимости оставлять что-то про запас («оставлять в гнезде одно яйцо»).",
+//     answer: "Крыша без дранки",
+//   },
+//   {
+//     number: 2,
+//     discription:
+//       "Деревянная обувь рядом с человеком, смотрящим сквозь пальцы, указывает на напрасное ожидание («тут стоят деревянные башмаки»)",
+//     answer: "иметь зубную боль за ушами",
+//   },
+//   {
+//     number: 3,
+//     discription:
+//       " Арбалетчик, выстреливший дважды («выстрелить вторым болтом, чтобы поймать первый»), повторяет глупое действие, а также расходует все запасы",
+//     answer: "расстрелять все стрелы",
+//   },
+//   {
+//     number: 4,
+//     discription:
+//       "Сцена с селянином, бегущим по полю за свиньями, скрывает сразу четыре крылатых выражения",
+//     answer: "бежать, будто задница горит",
+//   },
+//   {
+//     number: 5,
+//     discription:
+//       "Тут внимание привлекают юноша на вершине башни, размахивающий плащом, и старик рядом с ним, пускающий перья по ветру.",
+//     answer: "вешать плащ по ветру",
+//   },
+//   {
+//     number: 6,
+//     discription:
+//       "Пять пословиц зашифрованы на площадке, где столкнулись три человека и скот. Когда у кого-то настаёт чёрный период в жизни, говорят, что он «свалился с быка на ослиную задницу», а о том, кто «вешает на себя бремя» — что он преувеличивает свои неприятности.",
+//     answer: "один нищий жалеет, что другой нищий встал у его двери",
+//   },
+// ];
+
+// console.log(arrDiscription);
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  event.preventDefault();
+
+  let fragments = JSON.parse(json);
+  console.log(fragments);
+
+  let fragment = fragments[2];
+  let parentNode = document.createElement("div");
+  parentNode.classList.add("consult__form");
+
+  let titleNode = document.createElement("div");
+  titleNode.classList.add("consult__text");
+  titleNode.textContent = "Какое описание верно?";
+
+  let imageNode = document.createElement("div");
+  imageNode.classList.add("consult__image");
+
+  let fragmentNode = document.createElement("img");
+
+  fragmentNode.src = `${fragment.Image}`;
+
+  console.log(fragment.Image);
+  imageNode.append(fragmentNode);
+
+  let rowNode = document.createElement("div");
+  rowNode.classList.add("questions");
+
+  let closeNode = document.createElement("a");
+  closeNode.classList.add("close-popup");
+  closeNode.href = "#popup-close";
+  closeNode.textContent = `X`;
+
+  let answerNode = document.createElement("div");
+  answerNode.classList.add("consult-item__answer");
+
+  let detailsNode1 = document.createElement("details");
+  detailsNode1.classList.add("questions__item");
+
+  let summaryNode1 = document.createElement("summary");
+  summaryNode1.textContent = `${fragment.discriptionRight}`;
+
+  let backNode1 = document.createElement("p");
+  backNode1.textContent = `${fragment.back1}`;
+
+  detailsNode1.append(summaryNode1);
+  detailsNode1.append(backNode1);
+
+  let detailsNode2 = document.createElement("details");
+  detailsNode2.classList.add("questions__item");
+
+  let summaryNode2 = document.createElement("summary");
+  summaryNode2.textContent = `${fragment.discription2}`;
+
+  let backNode2 = document.createElement("p");
+  backNode2.textContent = `${fragment.back2}`;
+
+  detailsNode2.append(summaryNode2);
+  detailsNode2.append(backNode2);
+
+  let detailsNode3 = document.createElement("details");
+  detailsNode3.classList.add("questions__item");
+
+  let summaryNode3 = document.createElement("summary");
+  summaryNode3.textContent = `${fragment.discriptionRight}`;
+
+  let backNode3 = document.createElement("p");
+  backNode3.textContent = `${fragment.back3}`;
+
+  detailsNode3.append(summaryNode3);
+  detailsNode3.append(backNode3);
+
+  let detailsNode4 = document.createElement("details");
+  detailsNode4.classList.add("questions__item");
+
+  let summaryNode4 = document.createElement("summary");
+  summaryNode4.textContent = `${fragment.discription4}`;
+
+  let backNode4 = document.createElement("p");
+  backNode4.textContent = `${fragment.back1}`;
+
+  detailsNode4.append(summaryNode4);
+  detailsNode4.append(backNode4);
+
+  rowNode.append(detailsNode1);
+  rowNode.append(detailsNode2);
+  rowNode.append(detailsNode3);
+  rowNode.append(detailsNode4);
+
+  parentNode.append(titleNode);
+  parentNode.append(imageNode);
+  parentNode.append(answerNode);
+  parentNode.append(rowNode);
+  parentNode.append(closeNode);
+
+  detailsNode1.addEventListener("click", function Change() {
+    answerNode.textContent = `Вы ошиблись!`;
+  });
+
+  detailsNode2.addEventListener("click", function Change() {
+    imageNode.classList.toggle("consult-item__light");
+    answerNode.textContent = `Верно!`;
+  });
+
+  detailsNode3.addEventListener("click", function Change() {
+    answerNode.textContent = `Вы ошиблись!`;
+  });
+
+  detailsNode4.addEventListener("click", function Change() {
+    answerNode.textContent = `Вы ошиблись!`;
+  });
+
+  let popup = document.querySelector("#popup");
+  popup = popup.append(parentNode);
+});
